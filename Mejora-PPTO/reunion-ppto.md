e7e088e9802ad0c6d8d63eca2f76c179b5308a4e
Edgar Mercado G <emercadogarcia@gmail.com>
2023-09-29 09:43:07 -0400

    Actualizacion de registros y agregar tareas al 29/09/2023

---
+++ Mejora-PPTO/reunion-ppto.md
@@ -0,0 +1,10 @@
+# Reunion ppto - solicitud nueva
+Se requiere un presupuesto por quincena y por gestor.
+
+## Ver los datos desde la venta real de acuerdo a la hitoria
+* De acuerdo a la historia de compra del cliente.
+    * Referencia historica una año por productos.
+    * Obtener la fecuencia de compras.
+    * 
+* presupuesto minimo a clientes 350 bs
+* 
---
+++ send-mail/expediente-duplicado.sql
@@ -0,0 +1,33 @@
+/* Enivar email para notificar sobre expedientes con tareas duplicadas.*/
+
+DECLARE
+    html_content_inicio CLOB := '<html><style>* {font-family: sans-serif;}.content-table {border-collapse: collapse;margin: 25px 0;font-size: 0.9em; min-width: 400px;border-radius: 5px 5px 0 0;overflow: hidden;box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    }.content-table thead tr {background-color: #28367f;color: #ffffff;text-align: left;font-weight: bold;}.content-table th,.content-table td {padding: 12px 15px;}
+    .content-table tbody tr {border-bottom: 1px solid #dddddd;}.content-table tbody tr:nth-of-type(even) {background-color: #f3f3f3;
+    }.content-table tbody tr:last-of-type {border-bottom: 2px solid #f80000;}.content-table tbody tr.active-row {font-weight: bold;color: #f80000;}</style><body>';
+
+    html_content CLOB := '';
+    html_content_fin CLOB := '</body></html>';
+    v_resultado VARCHAR2(30);
+    cursor c_exp is select numero_expediente, CODIGO_SECUENCIA, count(numero_expediente) nro_tareas  from crmexpedientes_lin where empresa='004' AND STATUS_TAREA='01'   group by numero_expediente, CODIGO_SECUENCIA HAVING count(numero_expediente)>=2;
+    
+BEGIN
+    html_content := html_content_inicio || 'Estimado equipo de Tecnologia: <br/>A continuación se muestran los expedientes duplicados que figuran en el sistema:<hr/><table border="1" class="content-table"><thead><tr><th>Nro. expediente</th><th>Fecha</th><th>Descripción</th> <th>Típo Expediente</th></tr></thead><tbody>';
+
+    FOR BPM_EXP in c_exp LOOP
+        
+        html_content :=html_content||'<tr><td>'||TO_CHAR(BPM_EXP.NUMERO_EXPEDIENTE)||'</td><td>'||TO_CHAR(BPM_EXP.CODIGO_SECUENCIA)||'</td><td>'||TO_CHAR(BPM_EXP.nro_tareas)||'</td><td>';
+
+    END LOOP;
+        html_content := html_content || '</tbody></table>'|| html_content_fin;
+        PK_EMAIL.INICIALIZAR('BPM');
+        PK_EMAIL.SET_ASUNTO('[ALERTA] EXPEDIENTES DUPLICADOS QUE REQUIEREN ATENCION');
+        PK_EMAIL.SET_CUERPO_HTML(html_content);
+        
+        --PK_EMAIL.ADD_DESTINATARIO('TO', USUARIOS.CORREO);
+
+        --PK_EMAIL.ADD_DESTINATARIO('CC', 'daniel.lobo@promedical.com.bo,edgar.mercado@promedical.com.bo,marcelo.osinaga@promedical.com.bo');
+        PK_EMAIL.ADD_DESTINATARIO('TO', 'edgar.mercado@promedical.com.bo');
+        v_resultado := PK_EMAIL.ENVIAR();
+        html_content :='';
+END;
