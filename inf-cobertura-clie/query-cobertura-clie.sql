742a16f47c842c3158f8ce3a3289d280c7d107ff
Edgar Mercado G <emercadogarcia@gmail.com>
2024-07-25 09:24:38 -0400

    actualizaicon de datos codigo.

--- inf-cobertura-clie/query-cobertura-clie.sql
+++ inf-cobertura-clie/query-cobertura-clie.sql
@@ -3,121 +3,7 @@
 2. obtener los datso registrados en las ventas
 
  **/
-SELECT
-    C1,
-    C2,
-    C3,
-    C4,
-    C5,
-    C6,
-    C7,
-    C8,
-    C9,
-    C10,
-    C11,
-    C12,
-    C13,
-    C14,
-    C15,
-    C16,
-    C17,
-    C18,
-    C19,
-    C20,
-    C21,
-    C22,
-    C23,
-    C24,
-    C25,
-    C26,
-    C27,
-    C28,
-    C29,
-    C30,
-    C31,
-    C32,
-    C33,
-    C34,
-    C35,
-    C36,
-    C37,
-    C38,
-    C39,
-    C40,
-    C41,
-    C42,
-    C43,
-    C44,
-    C45,
-    C46,
-    C47,
-    C48,
-    C49,
-    C50,
-    N1,
-    N2,
-    N3,
-    N4,
-    N5,
-    N6,
-    N7,
-    N8,
-    N9,
-    N10,
-    N11,
-    N12,
-    N13,
-    N14,
-    N15,
-    N16,
-    N17,
-    N18,
-    N19,
-    N20,
-    N21,
-    N22,
-    N23,
-    N24,
-    N25,
-    N26,
-    N27,
-    N28,
-    N29,
-    N30,
-    N31,
-    N32,
-    N33,
-    N34,
-    N35,
-    N36,
-    N37,
-    N38,
-    N39,
-    N40,
-    N41,
-    N42,
-    N43,
-    N44,
-    N45,
-    N46,
-    N47,
-    N48,
-    N49,
-    N50,
-    TIPO,
-    COLUMNA_RUPTURA,
-    ALIAS_RUPTURA_C,
-    ALIAS_RUPTURA_N,
-    GI$COLOR
-FROM
-    (
-        SELECT
-            *
-        FROM
-            TABLE (
-                pk_b2b_geninf.gi_obtener_registros_pipe ('004', 'EMERCADO', 'BOL_VTAS_PPTO_2', '23')
-            )
-    )
+
 SELECT
     SUBSTR(bol_bi_vtas_ppto.reg, 1, 7) c1,
     SUBSTR(bol_bi_vtas_ppto.subreg, 1, 7) c2,
@@ -162,12 +48,12 @@ GROUP BY
     SUBSTR(bol_bi_vtas_ppto.rpn4, 1, 50),
     SUBSTR(bol_bi_vtas_ppto.rpn5, 1, 50),
     SUBSTR(bol_bi_vtas_ppto.rpn6, 1, 50)
-ORDER BY
-    1,
-    2
 
 
-SELECT
+
+SELECT /* query expandida  */
+    bol_bi_vtas_ppto.ejercicio,
+    bol_bi_vtas_ppto.v_mes,
     bol_bi_vtas_ppto.reg,
     bol_bi_vtas_ppto.subreg,
     bol_bi_vtas_ppto.cliente_id,
@@ -176,10 +62,9 @@ SELECT
     bol_bi_vtas_ppto.gestor_vtas,
     bol_bi_vtas_ppto.gestor_vtas_n,
     bol_bi_vtas_ppto.canalv,
+    1 v_efectiva,
     SUM(BOL_BI_VTAS_PPTO.CANTIDAD) cantidad,
     SUM(BOL_BI_VTAS_PPTO.IMP_NETO) IMP_NETO,
-    SUM(BOL_BI_VTAS_PPTO.PPTO_UND) PPTO_UND,
-    SUM(BOL_BI_VTAS_PPTO.PPTO_VLR) PPTO_VLR,
     TO_NUMBER(
         decode(
             SUM(BOL_BI_VTAS_PPTO.PPTO_VLR),
@@ -189,7 +74,74 @@ SELECT
             0,
             SUM(BOL_BI_VTAS_PPTO.IMP_NETO) / SUM(BOL_BI_VTAS_PPTO.PPTO_VLR) * 100
         )
-    ) cump
+    ) cump,
+    (SELECT 1 v_efectiva FROM BOL_BI_VTAS_PPTO X
+    WHERE X.ejercicio = bol_bi_vtas_ppto.ejercicio
+    AND X.V_MES= bol_bi_vtas_ppto.v_mes AND X.cliente_id=bol_bi_vtas_ppto.cliente_id
+    AND X.codigo_articulo NOT IN (
+        '00018653', '00018654', '00018656','00027574','00018812'
+    )
+    AND X.tipo_pedido BETWEEN '10' AND '16'
+    and X.cantidad>0 and X.uen in ('BOL - PROCAPS')
+    group by x.ejercicio,x.v_mes,x.cliente_id) VE_POP 
+FROM
+    BOL_BI_VTAS_PPTO 
+WHERE
+    bol_bi_vtas_ppto.ejercicio IN (2024)
+    AND bol_bi_vtas_ppto.v_mes = 07
+    AND bol_bi_vtas_ppto.codigo_articulo NOT IN (
+        '00018653',
+        '00018654',
+        '00018656',
+        '00027574',
+        '00018812'
+    )
+    AND bol_bi_vtas_ppto.tipo_pedido BETWEEN '10' AND '16'
+    and cantidad>0
+GROUP BY
+    bol_bi_vtas_ppto.ejercicio,
+    bol_bi_vtas_ppto.v_mes,
+    bol_bi_vtas_ppto.reg,
+    bol_bi_vtas_ppto.subreg,
+    bol_bi_vtas_ppto.cliente_id,
+    bol_bi_vtas_ppto.cliente_nombre,
+    bol_bi_vtas_ppto.cod_rpn,
+    bol_bi_vtas_ppto.gestor_vtas,
+    bol_bi_vtas_ppto.gestor_vtas_n,
+    bol_bi_vtas_ppto.canalv
+
+
+/**********************************************/
+SELECT 1 v_efectiva FROM BOL_BI_VTAS_PPTO X
+WHERE
+    X.ejercicio = bol_bi_vtas_ppto.ejercicio
+    AND X.V_MES= bol_bi_vtas_ppto.v_mes AND X.cliente_id=bol_bi_vtas_ppto.cliente_id
+    AND X.codigo_articulo NOT IN (
+        '00018653', '00018654', '00018656','00027574','00018812'
+    )
+    AND X.tipo_pedido BETWEEN '10' AND '16'
+    and X.cantidad>0 and x.uen in ('BOL - PROCAPS')
+
+select uen FROM
+    BOL_BI_VTAS_PPTO
+WHERE
+    bol_bi_vtas_ppto.ejercicio IN (2024)
+    AND bol_bi_vtas_ppto.v_mes = 07
+group by uen
+
+/**********************************************/
+
+SELECT /* query resumida*/
+    bol_bi_vtas_ppto.reg,
+    bol_bi_vtas_ppto.subreg,
+    bol_bi_vtas_ppto.cod_rpn,
+    bol_bi_vtas_ppto.gestor_vtas,
+    bol_bi_vtas_ppto.gestor_vtas_n,
+    bol_bi_vtas_ppto.canalv,
+    1 v_efectiva,
+    COUNT(cliente_id) nro_clie,
+    SUM(BOL_BI_VTAS_PPTO.CANTIDAD) cantidad,
+    SUM(BOL_BI_VTAS_PPTO.IMP_NETO) IMP_NETO
 FROM
     BOL_BI_VTAS_PPTO
 WHERE
@@ -203,15 +155,19 @@ WHERE
         '00018812'
     )
     AND bol_bi_vtas_ppto.tipo_pedido BETWEEN '10' AND '50'
+    and cantidad>0
 GROUP BY
     bol_bi_vtas_ppto.reg,
     bol_bi_vtas_ppto.subreg,
-    bol_bi_vtas_ppto.cliente_id,
-    bol_bi_vtas_ppto.cliente_nombre,
     bol_bi_vtas_ppto.cod_rpn,
     bol_bi_vtas_ppto.gestor_vtas,
     bol_bi_vtas_ppto.gestor_vtas_n,
-    bol_bi_vtas_ppto.canalv
-ORDER BY
-    12 DESC,
-    13 DESC
+    bol_bi_vtas_ppto.canalv 
+    
+ ORDER BY gestor_vtas_n
+/* campos retirados
+    bol_bi_vtas_ppto.cliente_id,
+    bol_bi_vtas_ppto.cliente_nombre,
+    bol_bi_vtas_ppto.cliente_id,
+    bol_bi_vtas_ppto.cliente_nombre,
+*/
